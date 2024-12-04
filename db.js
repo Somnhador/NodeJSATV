c597d1018f3c3941644882b636e0005cdf37fae7
Gabriel BS <178868428+Somnhador@users.noreply.github.com>
2024-12-04 06:33:01 -0300

    Terminado

---
+++ db.js
@@ -0,0 +1,71 @@
+const produtos = [
+    { 
+        id: 1,
+        nome:"Mouse",
+        quantidade:25,
+        preco:340.99
+    },
+    {
+        id: 2,
+        nome:"Teclado",
+        quantidade:3,
+        preco:180
+    },
+    {
+        id: 3,
+        nome:"HeadSet",
+        quantidade:70,
+        preco:202
+    },
+    {
+        id: 4,
+        nome:"Monitor",
+        quantidade:2,
+        preco:1417.85
+    },
+    {
+        id: 5,
+        nome:"Computador",
+        quantidade:8,
+        preco:4500
+    }
+];
+
+//Adiciona um novo produto
+function cadastrarProduto(produto){
+    produtos.push(produto);
+}
+
+//Lista de Produtos(Consulta todos os produtos)
+function listaDeProdutos(){
+    return produtos;
+}
+
+//Consulta produto por ID
+function consultarProduto(id){
+    return produtos.find(c => c.id == id);
+}
+
+function editarProduto(id,dados){
+    const prodNovo = produtos.find(c => c.id == id);
+   if (prodNovo){
+    prodNovo.nome =  dados.nome;
+    prodNovo.quantidade = dados.quantidade;
+    prodNovo.preco = dados.preco;
+   } else {
+        return("Erro!");
+   }    
+}
+
+function deletarProduto(id){
+    const indice = produtos.findIndex(c => c.id == id);
+    produtos.splice(indice,1);
+}
+
+module.exports = {
+    cadastrarProduto,
+    listaDeProdutos,
+    consultarProduto,
+    editarProduto,
+    deletarProduto
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const app = express();
+
+const db = require("./db");
+
+require("dotenv").config();
+
+app.use(express.json());
+
+app.post("/produtos", (request, response)=>{
+    const produto = request.body;
+    db.cadastrarProduto(produto);
+    response.sendStatus(201);
+});
+
+app.get("/produtos",(request, response)=>{
+    response.json(db.listaDeProdutos());
+});
+
+app.get("/produtos/:id",(request, response)=>{
+    const id = request.params.id;
+    response.json(db.consultarProduto(id));
+});
+
+app.patch("/produtos/:id",(request, response)=>{
+    const id = request.params.id;
+    const dados = request.body;
+    db.editarProduto(id, dados);
+    response.sendStatus(200);
+});
+
+app.delete("/produtos/:id",(request, response)=>{
+    const id = request.params.id;
+    db.deletarProduto(id);
+    response.sendStatus(204);
+});
+
+app.get("/", (request, response) => {
+    response.json({
+        message: "OK!"
+    })
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log("APP FUNCIONANDO") ;  
+});
